c82444c16afee12de15c54ac7f7c02b86f8ff295
Davide Colabove <davide.colabove@studenti.unipd.it>
2024-11-12 18:08:20 +0100

    Added main() in Queue.cpp

--- examples/Es11_ClassTemplate/Queue.cpp
+++ examples/Es11_ClassTemplate/Queue.cpp
@@ -1,6 +1,20 @@
+#include<iostream>
+using std::cout;
+using std::endl;
+#include "Queue.h"
+
 int main(){
-    //SOLO istanziazione ESPLICITA
-    Queue<int> qi;
-    Queue<boolletta> qb;
-    Queue<string> qs;
+    Queue<int>* pi = new Queue<int>;
+    // vengono istanziati la classe Queue<int> ed il suo costruttore Queue<int>() perche
+    // new deve costruire un oggetto della classe
+
+    int i;
+    for(i = 0; i < 10; ++i)
+        pi->add(i);
+    // vengono istanziati i metodi add<int> e empty<int>, la classe QueueItem<int> e il 
+    // suo costruttore QueueItem<int>()
+
+    for(i = 0; i < 10; ++i)
+        cout << pi->remove() << endl;
+    //viene istanziato il metodo remove<int> e il suo distruttore standard ~QueueItem<int>
 }
