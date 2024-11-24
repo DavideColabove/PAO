d7e3f053885866bd3f9339b407a3b240e6ef755b
Davide Colabove <davide.colabove@studenti.unipd.it>
2024-11-24 12:31:33 +0100

    Completed pol.cpp

--- examples/Es19_FigureGeometriche/pol.cpp
+++ examples/Es19_FigureGeometriche/pol.cpp
@@ -5,13 +5,37 @@ double punto::lung(const punto& p1, const punto& p2){
     return sqrt(((p2.x-p1.x)^2)-((p2.y-p1.y)^2));
 }
 
-poligono::poligono(unsigned int n, const punto v[]){}
+poligono::poligono(unsigned int n, const punto v[]){
+    nvertici = n;
+    pp = new punto[n];
+    for (int i = 0; i < n; i++) {
+        pp[i] = v[i];
+    }
+}
 
-poligono::~poligono(){}
+poligono::~poligono(){
+    delete[] pp;
+}
 
-poligono::poligono(const poligono& p){}
+poligono::poligono(const poligono& p){
+    nvertici = p.nvertici;
+    pp = new punto[nvertici];
+    for (int i = 0; i < nvertici; i++) {
+        pp[i] = p.pp[i];
+    }
+}
 
-poligono& poligono::operator=(const poligono& p){}
+poligono& poligono::operator=(const poligono& p){
+    if (this != &p) {
+        delete[] pp;
+        nvertici = p.nvertici;
+        pp = new punto[nvertici];
+        for (int i = 0; i < nvertici; i++) {
+            pp[i] = p.pp[i];
+        }
+    }
+    return *this;
+}
 
 double poligono::perimetro() const{
     int i;
